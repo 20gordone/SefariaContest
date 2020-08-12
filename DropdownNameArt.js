ec57cc4009a2794c0f5b844fcf10f07a142609c0
20gordone <ezrgordon@yahoo.com>
2020-08-12 11:19:55 -0400

    Add files via upload

--- DropdownNameArt.js
+++ DropdownNameArt.js
@@ -7,7 +7,6 @@ function hebTest(lookup, perek, book, fillVal){
                 addElement(lookup, book,String(perek),String(i+1),jqxhr.responseJSON.text[i])
             }
         }
-        console.log(fillVal)
         widthString = document.getElementById("myBar").style.width
         document.getElementById("myBar").style.width = (Number(widthString.substring(0,widthString.length-1))+fillVal) + "%"
         if (Number(widthString.substring(0,widthString.length-1))+fillVal>99){
@@ -44,7 +43,7 @@ function nameSearch(){
         alert("Please select some books to search");
         return;
     }
-    document.getElementById("vary").innerHTML ="<label for=\"versesdropdown\">Pasuk results:</label><select id=\"versesdropdown\"></select>"
+    document.getElementById("vary").innerHTML ="<label for=\"versesdropdown\"><i>Pasuk results:</i></label><select id=\"versesdropdown\" class=\"brownfill\"></select>"
     document.getElementById("searching").textContent = "Searching..."
     document.getElementById("loadbar").innerHTML = "<div id=\"myProgress\"><div id=\"myBar\"></div></div>"
     for (var i = 0; i<80; i+=2){
@@ -66,8 +65,8 @@ function noresults(){
         item.innerHTML = "Sorry, there don't seem to be any results for that search"
     }
     else if (item.children[item.children.length-1].id != "countresults"){
-            item.innerHTML += "<button id = \"noVowel\" onclick = \"fA2(this.parentElement.children[1].value)\">Render Without Vowels</button>"
-            item.innerHTML += "<button id = \"Vowel\" onclick = \"formatArt(this.parentElement.children[1].value)\">Render With Vowels</button>"
+            item.innerHTML += "<br> <button id = \"noVowel\" onclick = \"fA2(this.parentElement.children[1].value)\" class=\"brownfill\">Render Without Vowels</button>"
+            item.innerHTML += "<button id = \"Vowel\" onclick = \"formatArt(this.parentElement.children[1].value)\" class=\"brownfill\">Render With Vowels</button>"
             var resultCount = item.children[1].children.length
             if (resultCount == 1) {
                 item.innerHTML += "<br> <p id=\"countresults\"> Sorry, there was only one result. I hope it's a good one!</p>"
@@ -115,12 +114,16 @@ function addElement(lookup, book, chapter, verse, eText){
     element.target ="_blank"
     element.textContent = text
     var buttonId = lookup  + "." + book + "." + chapter + "." + verse
-    console.log(buttonId)
+    //console.log(buttonId)
     document.getElementById("versesdropdown").innerHTML += "<option value = \"" + buttonId + "\">" + text + "</option>"
 }
 
 //Includes trope/nikud
 function formatArt(sources){
+    var remover = document.getElementById("vary")
+    if (remover.children.length>0 && remover.children[remover.children.length-1].id == "countresults"){
+        document.getElementById("vary").removeChild(document.getElementById("countresults"))
+    }
     var source = sources.split(".");
     var lookup = source[0];
     var book = source[1];
@@ -135,23 +138,23 @@ function formatArt(sources){
                 simplePasuk += artPasuk.charAt(i)
         }
         var letInds = contained2(lookup, artPasuk) 
-        //imageSpace = document.getElementById("2" + sources)
         imageSpace = document.getElementById("artProduct")
-        console.log(artPasuk.charAt(letInds[0]))
         imageSpace.innerHTML = "<div><span>" + artPasuk.substring(letInds[0]+1,letInds[1]) + "</span><strong style=\"font-size:25px\">" + artPasuk.charAt(letInds[0]) + "</strong><span>" + artPasuk.substring(0,letInds[0]) + "</span></div>\n"
         letInds[letInds.length-1] = artPasuk.length
         for (var i = 2; i< letInds.length;i += 2){
             imageSpace.innerHTML += "<div><span>" + artPasuk.substring(letInds[i]+1,letInds[i+1]) + "</span><strong style=\"font-size:25px\">" + artPasuk.charAt(letInds[i]) + "</strong><span>" + artPasuk.substring(letInds[i-1]+1,letInds[i]) + " " + "</span></div>\n"
         }
-        imageSpace.innerHTML += "<br> <button id = \"1artProduct\" onclick = \"document.getElementById(this.id.substring(1,this.id.length)).innerHTML = this.id.substring(0,0)\">Clear Image</button>"
+        imageSpace.innerHTML += "<br> <button id = \"1artProduct\" onclick = \"document.getElementById(this.id.substring(1,this.id.length)).innerHTML = this.id.substring(0,0)\" class=\"brownfill\">Clear Image</button>"
     });
 }
 
 
 //No nikud/trope
 function fA2(sources){
-    //sources = sources.substring(sources.length)
-    console.log("blamo " + String(sources))
+    var remover = document.getElementById("vary")
+    if (remover.children.length>0 && remover.children[remover.children.length-1].id == "countresults"){
+        document.getElementById("vary").removeChild(document.getElementById("countresults"))
+    }
     var source = sources.split(".");
     var lookup = source[0];
     var book = source[1];
@@ -167,15 +170,13 @@ function fA2(sources){
         }
         artPasuk = simplePasuk
         var letInds = contained2(lookup, simplePasuk)
-        //imageSpace = document.getElementById("2" + sources)
         imageSpace = document.getElementById("artProduct")
-        console.log(artPasuk.charAt(letInds[0]))
         imageSpace.innerHTML = "<div><span>" + artPasuk.substring(letInds[0]+1,letInds[1]) + "</span><strong style=\"font-size:25px\">" + artPasuk.charAt(letInds[0]) + "</strong><span>" + artPasuk.substring(0,letInds[0]) + "</span></div>\n"
         letInds[letInds.length-1] = artPasuk.length
         for (var i = 2; i< letInds.length;i += 2){
             imageSpace.innerHTML += "<div><span>" + artPasuk.substring(letInds[i]+1,letInds[i+1]) + "</span><strong style=\"font-size:25px\">" + artPasuk.charAt(letInds[i]) + "</strong><span>" + artPasuk.substring(letInds[i-1]+1,letInds[i]) + " " + "</span></div>\n"
         }
-        imageSpace.innerHTML += "<br> <button id = \"1artProduct\" onclick = \"document.getElementById(this.id.substring(1,this.id.length)).innerHTML = this.id.substring(0,0)\">Clear Image</button>"
+        imageSpace.innerHTML += "<br> <button id = \"1artProduct\" onclick = \"document.getElementById(this.id.substring(1,this.id.length)).innerHTML = this.id.substring(0,0)\" class=\"brownfill\">Clear Image</button>"
     });
 }
 
@@ -204,4 +205,25 @@ function contained2(str1, verse){
 
     }
     return indices;
+}
+
+function checkTorah(){
+    tbuttons = document.getElementById('buttons 1').children;
+    for (var i = 0; i<9; i+=2){
+        tbuttons[i].checked = true
+    }
+}
+
+function checkProphets(){
+    tbuttons = document.getElementById('buttons 1').children;
+    for (var i = 11; i<52; i+=2){
+        tbuttons[i].checked = true
+    }
+}
+
+function checkWritings(){
+    tbuttons = document.getElementById('buttons 1').children;
+    for (var i = 54; i<79; i+=2){
+        tbuttons[i].checked = true
+    }
 }
